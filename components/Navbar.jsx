a331e202cd170c9d05d6291678aa8643b8e9b897
Niraj <nierforprogramming@gmail.com>
2026-03-11 16:02:03 +0545

    Navbar hide and show on scroll

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { RiMenu3Line } from "react-icons/ri";
 
@@ -7,9 +7,44 @@ import { sectionLinks } from "@/assets";
 import Logo from "./Logo";
 
 const Navbar = () => {
+  const [isBeingScrolled, setIsBeingScrolled] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    // determines last scrolling position
+    let lastScrollY = window.scrollY;
+
+    // finds if the user is scrolling or not
+
+    const controlNavbar = async () => {
+      const currentScrollY = window.scrollY;
+
+      // compare to find if the user is scrolling or not
+      // if the user has scrolled and scrolled more than 100px
+      if (currentScrollY > lastScrollY && currentScrollY > 300) {
+        // Being scrolled down  Navbar hides
+        setIsBeingScrolled(false);
+      } else {
+        // Means page is being scrolled up
+        // Therefore, showing navbar again
+        setIsBeingScrolled(true);
+      }
+
+      // Updating the scroll position
+      lastScrollY = currentScrollY;
+    };
+
+    // Triggering scroll event
+    window.addEventListener("scroll", controlNavbar);
+
+    // clean up when unmounted
+    return () => window.removeEventListener("scroll", controlNavbar);
+  }, []);
+
   return (
-    <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[90%] rounded-xl bg-black/30 backdrop-blur-md py-2 text-white">
+    <nav
+      className={`fixed top-4 left-1/2 -translate-x-1/2 z-50 w-[90%] rounded-xl bg-black/30 backdrop-blur-md py-2 text-white transition-transform duration-1000  ${isBeingScrolled ? "translate-y-0" : "-translate-y-50"} `}
+    >
       <div className="h-16 items-center flex justify-between">
         <Logo />
         {/* Desktop Menu */}
